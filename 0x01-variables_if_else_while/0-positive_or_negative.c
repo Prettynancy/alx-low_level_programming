3f80324f1eff08f13b0e8cc31ecf340367548777
Ukamaka <nancybaby1440@gmail.com>
2022-09-11 00:33:20 -0700

    variable

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -8,7 +8,7 @@
  */
 int main(void)
 {
-  int n;
+ int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > 0)
@@ -17,5 +17,5 @@ else if (n == 0)
 printf("%d is zero\n", n);
 else
 printf("%d is negative\n", n);
-   return (0);
+return (0);
 }
