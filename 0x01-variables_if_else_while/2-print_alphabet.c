099145e3b99af58c8767a2a9dd2cf388d8d1387f
Ukamaka <nancybaby1440@gmail.com>
2022-09-12 06:31:38 -0700

    variable

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,18 +1,30 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include <time.h>
-/**
- * main - Entry point
+#include <stdio.h>
+/*
+ * 2-print_alphabet.c
  *
- * Return: Always 0 (Success)
+ * Description:
+ * This program prints
+ * all the lower case characters in
+ * the alphabet, using putchar()
+ *
  */
+
+/**
+  * main - this is the main function that runs everything
+  * @void: no arguments passed in
+  *
+  * Return: 0
+  */
 int main(void)
 {
 char letter;
-int 1;
-letter = "a"
-for (1 = 0; i < 26; i++)
-putchar(letter + 1);
-putchar("\n");
+int i;
+
+letter = a;
+
+for (i = 0; i < 26; i++)
+putchar(letter + i);
+putchar(n);
 return (0);
 }
