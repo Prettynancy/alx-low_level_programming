afd5188ce55a92d9c79afbac6d6fe734361ca1af
Ukamaka <nancybaby1440@gmail.com>
2022-09-12 18:42:58 -0700

    variables

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,34 +1,34 @@
 #include <time.h>
 #include <stdio.h>
 /*
-* 3-print_alphabets.c 
-*
-* Description:
-* This program prints
-* all the lower case characters in
-* the alphabet, then the uppercase
-* using putchar()
-*
-*/
+ * 3-print_alphabets.c 
+ *
+ * Description:
+ * This program prints
+ * all the lower case characters in
+ * the alphabet, then the uppercase
+ * using putchar()
+ *
+ */
 
 /**
-* main - this is the main function that runs everything
-* @void: no arguments passed in
-*
-* Return: 0
-*/
+ * main - this is the main function that runs everything
+ * @void: no arguments passed in
+ *
+ * Return: 0
+ */
 int main(void)
 {
 	char letter;
 	int i;
 
-	letter = 'a';
+	letter = 'a';;
 
 	for (i = 0; i < 26; i++)
 		putchar(letter + i);
 	letter = 'A';
 	for (i = 0; i < 26; i++)
-		putchar(letter + i)
+		putchar(letter + i);
 	putchar('\n');
 	return (0);
-:}
+}
